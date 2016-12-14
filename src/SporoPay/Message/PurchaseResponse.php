ad0164f7916770c4706c012c35dde23026ec16e7
Tomas Majer <tomasmajer@gmail.com>
2016-12-14 15:51:44 +0100

    Fixed wrong case

--- src/SporoPay/Message/PurchaseResponse.php
+++ src/SporoPay/Message/PurchaseResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Omnipay\Sporopay\Message;
+namespace Omnipay\SporoPay\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
